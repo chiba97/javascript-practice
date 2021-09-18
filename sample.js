4d5a9bae15085d86f93c0ce8bb7dad65db11812c
chibakota <ktfkyrya@gmail.com>
2021-09-18 12:42:44 +0900

    add

--- sample.js
+++ sample.js
@@ -1,21 +1,84 @@
-var str = "webcamp"
+// var str = "webcamp"
 
-function foo() { 
-  console.log(str)
-  var str = "dmm webcamp"
-  console.log(str)
+// function foo() { 
+//   console.log(str)
+//   var str = "dmm webcamp"
+//   console.log(str)
+// }
+
+// foo()
+
+// function addString(strA){
+//   let addStr = "Hello" + strA;
+//   return addStr;
+// }
+
+// let alertString;
+// alertString = addString("WebCamp");
+
+// alert(alertString);
+
+// let promptStr = prompt("何か好きな文字を入力してください");
+// alert(promptStr)
+
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+// じゃんけんの手をランダムに作成する関数を呼び出す
+let js_hand = getJShand();
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+let judge = winLose(user_hand, js_hand);
+
+// 結果を表示する
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+// ランダムでじゃんけんの手を作成する関数
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+  let hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
 }
 
-foo()
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function winLose(user, js){
+  let winLoseStr;
 
-function addString(strA){
-  let addStr = "Hello" + strA;
-  return addStr;
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
 }
-
-let alertString;
-alertString = addString("WebCamp");
-
-alert(alertString);
-
-
